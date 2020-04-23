23d53716d09650f1b6b2226c609581a786e79409
kamuflazy <randrianaivo64@gmail.com>
2020-04-23 14:52:00 +0400

    le fichier clone dans ~/src

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -d "export PATH=$PATH:~/Documents/PSL/fancy_tools/bin" ]; then 
+if [[ -d "export PATH=$PATH:~/Documents/PSL/fancy_tools/bin" ]]; then 
 	echo "ce PATH existe deja"
 elif [[ $BRANCH != "master" ]]; then
 	echo "Dirrigez vous dans le branch master !"
@@ -10,4 +10,12 @@ else
 	echo "export PATH=$PATH:~/Documents/PSL/fancy_tools/bin">>~/.bashrc
 	cp -r ~/Documents/PSL/fancy_tools/bin  ~
 	echo "export PATH=$PATH:~/bin">>~/.bashrc
+	if [[ -e ~/src ]]; then
+		cp -r ~/Documents/PSL/fancy_tools/install.sh ~/src
+		echo "export PATH=$PATH:~/src">>~/.bashrc
+	else 
+		mkdir ~/src
+		cp -r ~/Documents/PSL/fancy_tools/install.sh ~/src
+		echo "export PATH=$PATH:~/src">>~/.bashrc
+	fi
 fi
